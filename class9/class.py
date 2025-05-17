056661cbef3197fe55c81df2cae3355f616bea2e
TTTTT113 <tomwang66168@gmail.com>
2025-05-17 11:07:06 +0800

    fix.hw

--- class9/class.py
+++ class9/class.py
@@ -27,16 +27,22 @@ while True:
 
 import random
 
+big = 100
+small = 1
 f = random.randint(1, 100)  # 從1到100的隨機數
 while True:
-    i = int(input("請輸入一個1到100的整數: "))
+    i = int(input(f"請輸入一個{small}到{big}的整數: "))
     if i == f:
         print("恭喜你，正確答案")
         break
     elif i > f:
         print("在小一點")
+        if i < big:
+            big = i
     elif i < f:
         print("在大一點")
+        if i > small:
+            small = i
 
 
 # list清單 (list)
@@ -101,28 +107,21 @@ for i in range(len(L)):  # 這邊的i是index
 # 要使用哪一種方式讀取List當中的資料, 要看使用的情境當會不會需要index
 
 
-j = juice_list = [".蘋果汁, 柳橙汁, 葡萄汁,系統關閉"]
-print(j)
+j = juice = [".蘋果汁, 柳橙汁, 葡萄汁,系統關閉"]
 
 
-"""
-回家作業1
-修改上課的練習程式，當使用者輸入數值時，程式不僅提示再大再小還需要提示縮小過後的輸入範圍
-EX:
-請輸入0~100的整數:50
-再小一點
-請輸入0~50的整數:25
-再小一點
-請輸入0~25的整數:15
-再大一點
-請輸入15~25的整數:30
-再小一點
-請輸入15~25的整數:10
-再大一點
-請輸入15~25的整數:20
-再大一點
-請輸入20~25的整數:23
-再大一點
-請輸入23~25的整數:24
-恭喜猜中!
-"""
+while True:
+    for i in range(len(juice)):
+        print(f"{i + 1}.{juice[i]}")
+    try:
+        n = int(input("請輸入編號"))
+    except:
+        print("輸入錯誤查無此商品,請重新輸入")
+        continue
+    if n == len(juice):
+        print("系統關閉")
+        break
+    elif 1 <= n < len(juice):
+        print(f"您點的商品是{juice[n - 1]}")
+    else:
+        print("輸入錯誤查無此商品,請重新輸入")
--- class9/tempCodeRunnerFile.py
+++ class9/tempCodeRunnerFile.py
@@ -1,2 +1,18 @@
-j = juice_list = [".蘋果汁, 柳橙汁, 葡萄汁,系統關閉"]
-print(j)
+import random
+
+big = 100
+small = 1
+f = random.randint(1, 100)  # 從1到100的隨機數
+while True:
+    i = int(input(f"請輸入一個{small}到{big}的整數: "))
+    if i == f:
+        print("恭喜你，正確答案")
+        break
+    elif i > f:
+        print("在小一點")
+        if i < big:
+            big = i
+    elif i < f:
+        print("在大一點")
+        if i > small:
+            small = i
